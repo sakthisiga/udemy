5e79568daee2cb9be4aa761dec64f2a3ae3a24c0
Sakthivel Deivasigamani <sakthisiga@gmail.com>
2015-10-20 22:27:31 -0700

    SQL Updated on 10-20

--- sql/udemy_db.sql
+++ sql/udemy_db.sql
@@ -1,59 +1,72 @@
--- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Oct 21, 2015 at 03:28 AM
--- Server version: 5.6.26
--- PHP Version: 5.5.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `udemy_db`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.0.17-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.3.0.4984
 -- --------------------------------------------------------
 
---
--- Table structure for table `user`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE IF NOT EXISTS `user` (
+-- Dumping database structure for udemy_db
+CREATE DATABASE IF NOT EXISTS `udemy_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `udemy_db`;
+
+
+-- Dumping structure for table udemy_db.note
+CREATE TABLE IF NOT EXISTS `note` (
+  `note_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(5) unsigned NOT NULL,
-  `username` varchar(75) NOT NULL DEFAULT '0',
-  `password` varchar(75) NOT NULL DEFAULT '0',
-  `email` varchar(75) NOT NULL DEFAULT '0',
-  `date_added` date NOT NULL DEFAULT '0000-00-00',
-  `date_modified` date NOT NULL DEFAULT '0000-00-00'
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_modified` datetime NOT NULL,
+  PRIMARY KEY (`note_id`),
+  KEY `FK_note_user` (`user_id`),
+  CONSTRAINT `FK_note_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+-- Dumping data for table udemy_db.note: ~0 rows (approximately)
+/*!40000 ALTER TABLE `note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `note` ENABLE KEYS */;
 
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+-- Dumping structure for table udemy_db.todo
+CREATE TABLE IF NOT EXISTS `todo` (
+  `todo_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(5) unsigned NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `completed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `date_added` datetime NOT NULL,
+  `date_modified` datetime NOT NULL,
+  PRIMARY KEY (`todo_id`),
+  KEY `FK_todo_user` (`user_id`),
+  CONSTRAINT `FK_todo_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(5) unsigned NOT NULL AUTO_INCREMENT;
+-- Dumping data for table udemy_db.todo: ~0 rows (approximately)
+/*!40000 ALTER TABLE `todo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `todo` ENABLE KEYS */;
+
+
+-- Dumping structure for table udemy_db.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(75) NOT NULL,
+  `password` varchar(75) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_modified` datetime NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table udemy_db.user: ~1 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `date_added`, `date_modified`) VALUES
+	(1, 'sakthi', 'cee1f86a3b75f70e97b71ad01e821a80', 'sakthisiga@outlook.com', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
